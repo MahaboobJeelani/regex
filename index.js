7d48c0250edd45191c4085b24a29a99da85dfbe9
mahaboob <mahaboob@murphi.ai>
2024-12-27 18:01:06 +0530

    Quantifiers

--- index.js
+++ index.js
@@ -47,3 +47,68 @@ const regex = /[A-Za-z]/;
 const str = "hello 123!";
 console.log(str.match(regex));
 
+// Quantifiers
+// The a* matches zero or more occurrences of a. It greedily matches all as in the string.
+const regex = /a*/
+const str = "aaa"
+console.log(str.match(regex));
+
+// Greedy by default: Matches at least one occurrence and as many as possible.
+// Explanation: The a+ matches one or more as. It matches the entire "aaa".
+const regex = /a+/
+const str = "aaa"
+console.log(str.match(regex));
+
+// Greedy by default: Matches zero or one occurrence.
+// Explanation: The a? matches zero or one a. It matches "bad" because a is present.
+const regex = /ba?d/
+const str = "bad"
+console.log(str.match(regex));
+
+// (Matches exactly n times)
+// Explanation: The a{3} matches exactly three as. It matches "aaa".
+const regex = /a{3}b{3}/
+const str = "aaabbbbccc"
+console.log(str.match(regex));
+
+// {n,} (Matches at least n times)
+// Explanation: The a{2,} matches at least two as. It matches all "aaa".
+const regex = /a{2,}/
+const str = "aaa"
+console.log(str.match(regex));
+
+// {n,m} (Matches between n and m times)
+// Explanation: The a{1,2} matches between 1 and 2 as. It matches the first two as.
+const regex = /a{1,3}/
+const str = "aaaaa"
+console.log(str.match(regex));
+
+// Explanation: The a*? matches zero or more as, but lazily. It stops at the first possible match: the empty string before the first a.
+const regex = /a*?/
+const str = "aaa"
+console.log(str.match(regex));
+
+// Explanation: The a+? matches one or more as, but lazily. It stops at the first a.
+const regex = /ab+?/
+const str = "abbb"
+console.log(str.match(regex));
+
+// Explanation: The a?? matches zero or one a, but lazily. It prefers zero occurrences, so it matches "bd"
+const regex = /ba??d/;
+const str = "bd";
+console.log(str.match(regex));
+
+// Explanation: The a{1,3}? matches between 1 and 3 as, but lazily. It stops at the minimum match: one a.
+const regex = /a{1,3}?/;
+const str = "aaa";
+console.log(str.match(regex)); 
+
+// Explanation: The .* greedily matches everything between the first and last quotes.
+const regex = /".*"/;
+const str = '"hello" "world"';
+console.log(str.match(regex)); 
+
+// Explanation: The .*? lazily matches the fewest characters, stopping at the first closing quote.
+const regex = /".*?"/;
+const str = '"hello" "world"';
+console.log(str.match(regex));
